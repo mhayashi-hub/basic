4b259f3d2245f38a2512b5287e71447f00c79c32
mhayashi-hub <redbrick.gmad@gmail.com>
2021-10-07 14:27:38 +0900

    2021/10/07 実習時途中結果3

--- src/intro/sample/DoubleLoop.java
+++ src/intro/sample/DoubleLoop.java
@@ -19,7 +19,8 @@ public class DoubleLoop {
 
         for (int i =0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                System.out.println(array[i][j]);
+                System.out.println("i,jの順番の場合："+array[i][j]);
+                System.out.println("j,iの順番の場合："+array[j][i]);
             }
 
         }
